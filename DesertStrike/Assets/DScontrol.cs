3b282aec27fa4b12d628d667f636af9d518eb1b8
Solomon Vromans <svromans@gmail.com>
2013-08-15 15:59:20 -0300

    Started Space Invaders Project

---
+++ DesertStrike/Assets/DScontrol.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DScontrol : MonoBehaviour {
+	float speed = 10;
+	
+	public GameObject projectilePrefab;
+	
+	// Update is called once per frame
+	void Update () {
+		float xaxis = Input.GetAxis("Horizontal");		
+		transform.Translate(new Vector3(xaxis * Time.deltaTime * speed, 0, 0));
+		if (Input.GetKeyDown(KeyCode.Space)){
+			Instantiate(projectilePrefab,transform.position,Quaternion.identity);
+		}
+	
+	}
+}
---
+++ DesertStrike/Assets/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	float speed = 10;
+	float startTime = 0;
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(transform.up * speed * Time.deltaTime);
+		
+		if (Time.time - startTime > 2){
+			Destroy(gameObject);
+		}
+		
+		RaycastHit hit;
+		if(Physics.Raycast(transform.position,-transform.up * Time.deltaTime, out hit,0.2f)){
+			if(hit.transform.name != "Shup"){
+			Destroy(gameObject);
+			Destroy(hit.transform.gameObject);
+			}
+		}
+	}
+	
+
+}
